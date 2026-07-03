6f96e23c60542de7425528e4ce9c156b5037b85c
Codemaster <requestse@gmail.com>
2026-07-03 21:59:57 +0300

    Added regex and matchesWith support

--- plugins/input-validation.js
+++ plugins/input-validation.js
@@ -1,13 +1,25 @@
-NS.validate = (selector, options = {}) => {
+NS.validate = (selector, type = "value", options = {}) => {
     if (!selector) return console.error("You must pass a selector.");
+    type = type === "text" ? "textContent" : "value"
 
     const foundElement = document.querySelector(selector);
-    if (options?.required && !foundElement.value) return false;
-    if (options?.enum && !options?.enum.includes(foundElement.value)) return false;
+    if (options?.required && !foundElement[type]) return false;
+    if (options?.regex && !options?.regex.test(foundElement[type])) return false;
+    if (options?.enum && !options?.enum.includes(foundElement[type])) return false;
+    if (options?.matchesWith) { // Both must match the type, case sensitive.
+        const matchElementValue = document.querySelector(options?.matchesWith);
+        if (!matchElement) {
+            console.error(`Element "${options.matchesWith}" not found for matchesWith validation.`);
+            return false;
+        }
+
+        if (matchElementValue[type] !== foundElement[type]) return false;
+    }
+
     if (foundElement.type === "number") {
-        if (!Number.isInteger(Number(foundElement.value))) return false;
-        if (options?.max !== undefined && foundElement.value > options?.max) return false;
-        if (options?.min !== undefined && foundElement.value < options?.min) return false;
+        if (!Number.isInteger(Number(foundElement[type]))) return false;
+        if (options?.max !== undefined && foundElement[type] > options?.max) return false;
+        if (options?.min !== undefined && foundElement[type] < options?.min) return false;
     }
 
     return true;
